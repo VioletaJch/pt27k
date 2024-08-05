a9f18fcab59d69c7b6980d061f649ed5f627b493
VioletaJch <violeta.jch@icloud.com>
2024-08-05 19:22:29 +0300

    adding lection2.py

---
+++ exercises/lection2.py
@@ -0,0 +1,38 @@
+# python Sets
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {1, 5, 7, 9, 3}
+# set3 = {True, False, False}
+
+# print(set1)
+# print(set2)
+# print(set3)
+
+
+# python tuples
+
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple)
+
+# python list
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+
+# python dictionary
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict)
+
+# Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict["brand"])
