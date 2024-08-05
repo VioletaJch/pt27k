76a45cb7213be65e0bcbc8f860140676b7a887d0
VioletaJch <violeta.jch@icloud.com>
2024-08-05 19:32:29 +0300

    adding main.py

---
+++ exercises/main.py
@@ -0,0 +1,46 @@
+name = "Code Academy"
+print(name[5])
+
+# A
+
+print(name[-2])
+
+# m
+
+print(name[5:12])
+
+# Academy
+
+print(name[5:])
+
+# Academy
+
+print(name[:4])
+
+# Code
+
+print(name[5:12:1])
+
+# Academy
+
+print(name[5::2])
+
+# Aaey
+
+print(name[::-1])
+
+# ymedacA edoC
+
+print(name.split())
+
+# ['Code', 'Academy']
+
+print(name.upper())
+
+# CODE ACADEMY
+
+print(name.replace('c', 'k'))
+
+# Code Akademy
+
+print(name.replace('Code', 'Music'))
